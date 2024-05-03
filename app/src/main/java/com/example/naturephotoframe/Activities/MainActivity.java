1629e71ba71596fb79312e98a05ced7f95c7deba
Junaid-Khan <80524662+JunaidKhan44@users.noreply.github.com>
2024-05-03 14:29:13 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/naturephotoframe/Activities/MainActivity.java
+++ app/src/main/java/com/example/naturephotoframe/Activities/MainActivity.java
@@ -81,38 +81,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void openCamera() {
 
-     /*   String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "Image_" + timeStamp;
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        try {
-            File image = File.createTempFile(imageFileName, ".jpg", storageDir);
-            currentPhotoPath = image.getAbsolutePath();
-            Uri photoURI = FileProvider.getUriForFile(this,
-                    "com.example.naturephotoframe.fileprovider", image);
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-            startActivityForResult(takePictureIntent, 7);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
-
         performCrop();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-     /*   if (requestCode == Result_Gallery_Request_Code && resultCode == RESULT_OK && data != null) {
-            Common.galleryImageUri = data.getData();
-            imageView.setImageURI(Common.galleryImageUri);
-        }
-
-        if (requestCode == 7 && resultCode == RESULT_OK) {
-            Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath);
-            Log.i("ImageUri", "onActivityResult: "+ bitmap.toString());
-            imageView.setImageBitmap(bitmap);
-        }*/
 
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
@@ -148,52 +122,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void performCrop() {
-        // start picker to get image for cropping and then use the image in cropping activity
+  
         CropImage.activity()
                 .setGuidelines(CropImageView.Guidelines.ON)
                 .start(this);
     }
 
-    /*
-     * saves image to camera gallery
-     * */
-    /*private void saveImageToGallery() {
-        Dexter.withContext(this).withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                .withListener(new MultiplePermissionsListener() {
-                    @Override
-                    public void onPermissionsChecked(MultiplePermissionsReport report) {
-                        if (report.areAllPermissionsGranted()) {
-                            final String path = BitmapUtils.insertImage(getContentResolver(), finalImage, System.currentTimeMillis() + "_profile.jpg", null);
-                            if (!TextUtils.isEmpty(path)) {
-                                Snackbar snackbar = Snackbar
-                                        .make(coordinatorLayout, "Image saved to gallery!", Snackbar.LENGTH_LONG)
-                                        .setAction("OPEN", new View.OnClickListener() {
-                                            @Override
-                                            public void onClick(View view) {
-                                                openImage(path);
-                                            }
-                                        });
-
-                                snackbar.show();
-                            } else {
-                                Snackbar snackbar = Snackbar
-                                        .make(coordinatorLayout, "Unable to save image!", Snackbar.LENGTH_LONG);
-
-                                snackbar.show();
-                            }
-                        } else {
-                            Toast.makeText(getApplicationContext(), "Permissions are not granted!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
-                        token.continuePermissionRequest();
-                    }
-                }).check();
-
-    }*/
-    // opening image in default image viewer app
+    
     private void openImage(String path) {
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_VIEW);
